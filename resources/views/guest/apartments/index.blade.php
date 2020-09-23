fa5f5c7510afbae350d573073fa05982fb453419
Marco Prosperi <marcoprosperi@outlook.com>
2020-09-23 18:09:52 +0200

    Guest apartments index + apartment controllerr

--- resources/views/guest/apartments/index.blade.php
+++ resources/views/guest/apartments/index.blade.php
@@ -1,1 +1,45 @@
-<h1>ciao sono index di guest->apartments</h1>
+{{-- @extends('') --}}
+{{-- @section('') --}}
+
+{{-- inizio sezione ricerca --}}
+<section>
+  <form class="" action="" method="get">
+    <label class="sr-only" for="">Ricerca un appartamento</label>
+    <input type="text" name="" value="">
+  </form>
+
+</section>
+{{-- fine sezione ricerca --}}
+
+{{-- inizio sezione appartamenti --}}
+<section>
+
+  <div>
+    <h3>Servizi</h3>
+    <ul>
+      @foreach ($services as $service)
+        <input type="checkbox" name="services[]" value="{{$service->id}}">
+        <span>{{$service->name}}</span>
+      @endforeach
+    </ul>
+  </div>
+
+
+  @if (!$apartments->isEmpty())
+    <ul>
+      @foreach ($apartments as $apartment)
+        <li>
+          {{$apartment->title}}
+          <div>
+            <a class="btn btn-primary" href="{{route('apartments.show', $apartment)}}">Visualizza</a>
+          </div>
+        </li>
+      @endforeach
+    </ul>
+  @endif
+
+</section>
+{{-- fine sezione appartamenti --}}
+
+
+{{-- @endsection --}}
