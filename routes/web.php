2407e1e30b34f2dc9a3d0215844846c4a378bdff
Alessandro <alex.ixs@hotmail.com>
2020-09-23 15:50:55 +0200

    Creato Admin/ApartmentsCRUD e le route impostate

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('homepage');
+})->name('homepage');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/guest/apartments', 'ApartmentController@index')->name('apartments.index');
+Route::get('/guest/apartments/{apartment}', 'ApartmentController@show')->name('apartments.show');
+
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->middleware('auth')
+    ->name('admin.')
+    ->group(function() {
+        Route::resource('apartments', 'ApartmentController');
+    });
