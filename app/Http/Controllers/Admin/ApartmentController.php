e335e776ae2192d9db9b14f84f2ed050e074c704
Alessandro <alex.ixs@hotmail.com>
2020-09-23 18:37:56 +0200

    Nav links per utenti loggati

--- app/Http/Controllers/Admin/ApartmentController.php
+++ app/Http/Controllers/Admin/ApartmentController.php
@@ -30,7 +30,7 @@ class ApartmentController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.apartments.create');
     }
 
     /**
--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -32,6 +32,12 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a href="{{route('admin.apartments.create')}}" class="dropdown-item">
+                                Add apartment
+                            </a>
+                            <a href="{{route('admin.apartments.index')}}" class="dropdown-item">
+                                Your appartments
+                            </a>
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
