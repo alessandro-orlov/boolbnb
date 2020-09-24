b0f079d51c8d9191a93ab7bfdf13bffad34395ec
SaraCremonesi <saracrem00@gmail.com>
2020-09-24 09:58:25 +0200

    aggiornamento footer

--- public/js/app.js
+++ public/js/app.js
@@ -37320,19 +37320,31 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Can't find stylesheet to import.\n  ╷\n5 │ @import 'variables';\r\n  │         ^^^^^^^^^^^\n  ╵\n  C:\\xampp\\htdocs\\boolbnb\\resources\\sass\\app.scss 5:9  root stylesheet\n    at C:\\xampp\\htdocs\\boolbnb\\node_modules\\webpack\\lib\\NormalModule.js:316:20\n    at C:\\xampp\\htdocs\\boolbnb\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at C:\\xampp\\htdocs\\boolbnb\\node_modules\\loader-runner\\lib\\LoaderRunner.js:233:18\n    at context.callback (C:\\xampp\\htdocs\\boolbnb\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass-loader\\dist\\index.js:73:7\n    at Function.call$2 (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:88844:16)\n    at _render_closure1.call$2 (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:78084:12)\n    at _RootZone.runBinary$3$3 (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:26377:18)\n    at _RootZone.runBinary$3 (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:26381:19)\n    at _FutureListener.handleError$1 (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:24870:19)\n    at _Future__propagateToListeners_handleError.call$0 (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:25162:49)\n    at Object._Future__propagateToListeners (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:4489:77)\n    at _Future._completeError$2 (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:24996:9)\n    at _AsyncAwaitCompleter.completeError$2 (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:24358:12)\n    at Object._asyncRethrow (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:4238:17)\n    at C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:14330:20\n    at _wrapJsFunctionForAsync_closure.$protected (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:4263:15)\n    at _wrapJsFunctionForAsync_closure.call$2 (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:24379:12)\n    at _awaitOnObject_closure0.call$2 (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:24371:25)\n    at _RootZone.runBinary$3$3 (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:26377:18)\n    at _RootZone.runBinary$3 (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:26381:19)\n    at _FutureListener.handleError$1 (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:24870:19)\n    at _Future__propagateToListeners_handleError.call$0 (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:25162:49)\n    at Object._Future__propagateToListeners (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:4489:77)\n    at _Future._completeError$2 (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:24996:9)\n    at _AsyncAwaitCompleter.completeError$2 (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:24358:12)\n    at Object._asyncRethrow (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:4238:17)\n    at C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:16928:20\n    at _wrapJsFunctionForAsync_closure.$protected (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:4263:15)\n    at _wrapJsFunctionForAsync_closure.call$2 (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:24379:12)\n    at _awaitOnObject_closure0.call$2 (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:24371:25)\n    at _RootZone.runBinary$3$3 (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:26377:18)\n    at _RootZone.runBinary$3 (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:26381:19)\n    at _FutureListener.handleError$1 (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:24870:19)\n    at _Future__propagateToListeners_handleError.call$0 (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:25162:49)\n    at Object._Future__propagateToListeners (C:\\xampp\\htdocs\\boolbnb\\node_modules\\sass\\sass.dart.js:4489:77)");
+
+/***/ }),
+
+/***/ "./resources/sass/boolbnb/app.scss":
+/*!*****************************************!*\
+  !*** ./resources/sass/boolbnb/app.scss ***!
+  \*****************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
 // removed by extract-text-webpack-plugin
 
 /***/ }),
 
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!***********************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/boolbnb/app.scss ***!
+  \***********************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Alfa\Desktop\Boolean Project\Final project\BoolBnb\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Alfa\Desktop\Boolean Project\Final project\BoolBnb\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\boolbnb\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! C:\xampp\htdocs\boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\boolbnb\resources\sass\boolbnb\app.scss */"./resources/sass/boolbnb/app.scss");
 
 
 /***/ })
