61d2cccec9a50930e6d6d12f9958367c9de90f73
Alessandro <alex.ixs@hotmail.com>
2020-10-10 16:10:41 +0200

    aboute page updated

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,25 +8,47 @@
   <div class="container py-5">
     <div class="about-container">
       <div class="app-description">
-        <h1>L'app</h1>
-        <p>E' una riproduzione rivisitata di uno dei più famosi portali online che mette in contatto persone in cerca di un alloggio o di una camera per brevi periodi, con persone che dispongono di spazi extra da affittare, generalmente privati.</p>
-        <h2>Funzionalità generali</h2>
-        <h3><i class="fas fa-map-marker-alt"></i>Geolocalizzazione</h3>
-        <p>Attraverso il software algolia è possibile geolocalizzare sulla mappa del territorio i vari appartamenti disponibili</p>
-        <h3><i class="fas fa-user-cog"></i>Gestione appartamenti</h3>
-        <p>Modifica e aggiorna gli appartamenti inseriti</p>
-        <h3><i class="fas fa-comment-dots"></i>Messaggistica</h3>
-        <p>Contatta i proprietari degli appartamenti da affittare</p>
-        <h3><i class="fas fa-chart-bar"></i>Statistiche</h3>
-        <p>Calcola quante visite ha ricevuto ogni appartamento con cadenza mensile, settimanale e giornaliera</p>
-        <h3><i class="fas fa-star"></i>Sponsor</h3>
-        <p>Possibilità di sponsorizzare uno o più appartamenti inseriti, per un certo periodo di tempo, in modo da aumentarne la visibilità</p>
-        <h3><i class="fas fa-money-check-alt"></i></i>Pagamenti</h3>
-        <p>Prenotazioni online in sicurezza grazie a Braintree</p>
+        <h1>Boolbnb</h1>
+        <p>
+          BoolBnB è una applicazione per trovare e gestire l’affitto di appartamenti.<br>
+          E' una riproduzione rivisitata di uno dei più famosi portali online che mette in contatto persone in cerca di un alloggio o di una camera per brevi periodi, con persone che dispongono di spazi extra da affittare, generalmente privati.</p>
+        <h2>Functionalities</h2>
+        <div class="functionalities">
+          <div>
+            <h3><i class="fas fa-map-marker-alt"></i>Geolocalizzazione</h3>
+            <p>Attraverso il software algolia è possibile geolocalizzare sulla mappa del territorio i vari appartamenti disponibili</p>
+          </div>
+
+          <div>
+            <h3><i class="fas fa-user-cog"></i>Gestione appartamenti</h3>
+            <p>Modifica e aggiorna gli appartamenti inseriti</p>
+          </div>
+
+          <div>
+            <h3><i class="fas fa-comment-dots"></i>Messaggistica</h3>
+            <p>Contatta i proprietari degli appartamenti da affittare</p>
+          </div>
+
+          <div>
+            <h3><i class="fas fa-chart-bar"></i>Statistiche</h3>
+            <p>Calcola quante visite ha ricevuto ogni appartamento con cadenza mensile, settimanale e giornaliera</p>
+          </div>
+
+          <div>
+            <h3><i class="fas fa-star"></i>Sponsor</h3>
+            <p>Possibilità di sponsorizzare uno o più appartamenti inseriti, per un certo periodo di tempo, in modo da aumentarne la visibilità</p>
+          </div>
+
+          <div>
+            <h3><i class="fas fa-money-check-alt"></i></i>Pagamenti</h3>
+            <p>Prenotazioni online in sicurezza grazie a Braintree</p>
+          </div>
+        </div>
+
       </div>
-      <h1>I creatori</h1>
-      <div class="row">
-        <div class="col-lg-3 col-sm-6">
+      <h2 class="creators-heading">Developed by</h2>
+      <div class="creators">
+        {{-- <div class="creator"> --}}
           <div class="box-about">
             <div class="picture">
               <a href="#">
@@ -43,8 +65,8 @@
               </p>
             </div>
           </div>
-        </div>
-        <div class="col-lg-3 col-sm-6">
+        {{-- </div> --}}
+        {{-- <div class="creator"> --}}
           <div class="box-about">
             <div class="picture">
               <a href="#">
@@ -61,8 +83,8 @@
               </p>
             </div>
           </div>
-        </div>
-        <div class="col-lg-3 col-sm-6">
+        {{-- </div> --}}
+        {{-- <div class="creator"> --}}
           <div class="box-about">
             <div class="picture">
               <a href="#">
@@ -79,8 +101,8 @@
               </p>
             </div>
           </div>
-        </div>
-        <div class="col-lg-3 col-sm-6">
+        {{-- </div> --}}
+        {{-- <div class="creator"> --}}
           <div class="box-about">
             <div class="picture">
               <a href="#">
@@ -97,7 +119,7 @@
               </p>
             </div>
           </div>
-        </div>
+        {{-- </div> --}}
       </div>
     </div>
   </div>
